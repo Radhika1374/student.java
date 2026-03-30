d4bc2d131a59df1f80ad58995ce51aed34b281ae
Radhika1374 <radhikanampally07@gmail.com>
2026-03-30 11:18:10 +0530

    updated

---
+++ student.java
@@ -0,0 +1,9 @@
+int a=10;
+int b=20;
+
+public class student {
+    public static void main(String[] args) {
+        int sum=a+b;
+        System.out.println("The sum of a and b is: " + sum);
+    }
+}
