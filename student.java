ac1e3074490a74eca100f7c5180a0c4a41db75d5
Radhika1374 <radhikanampally07@gmail.com>
2026-03-30 11:56:04 +0530

    some changes has done

--- student.java
+++ student.java
@@ -1,9 +1,9 @@
 int a=10;
-int b=20;
 
 public class student {
     public static void main(String[] args) {
-        int sum=a+b;
-        System.out.println("The sum of a and b is: " + sum);
+        int factorila=factorial*a;
+
+        System.out.println("Factorial of "+a+" is: "+factorila);
     }
 }
